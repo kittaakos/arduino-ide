cd8f7e659416e0fbc2385b7075ab38c7a0e46a3e
Akos Kitta <a.kitta@arduino.cc>
2022-07-25 10:12:59 +0200

    Use DI in theme service to handle the default IDE2 theme.
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- electron/packager/index.js
+++ electron/packager/index.js
@@ -52,8 +52,7 @@
     'patch',
     'resources',
     'scripts',
-    'template-package.json',
-    'webpack.config.js'
+    'template-package.json'
   ];
   fs.readdirSync(path('..', 'build'))
     .filter((filename) => resourcesToKeep.indexOf(filename) === -1)
