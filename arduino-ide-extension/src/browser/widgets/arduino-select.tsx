ffb937e3e4d7fe6ab179b0cb8ddd849bd8de5267
Akos Kitta <a.kitta@arduino.cc>
2022-11-08 16:46:56 +0100

    fix: custom select compiles
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/browser/widgets/arduino-select.tsx
+++ arduino-ide-extension/src/browser/widgets/arduino-select.tsx
@@ -1,61 +1,61 @@
 import * as React from '@theia/core/shared/react';
-import Select, { Props } from 'react-select/dist/declarations/src/Select';
+import Select from 'react-select';
 import { StylesConfig } from 'react-select/dist/declarations/src/styles';
-import { GroupBase } from 'react-select/dist/declarations/src/types';
 import { ThemeConfig } from 'react-select/dist/declarations/src/theme';
+import { GroupBase } from 'react-select/dist/declarations/src/types';
+import { StateManagerProps } from 'react-select/dist/declarations/src/useStateManager';
+
+const controlHeight = 27; // from `monitor.css` -> `.serial-monitor-container .head` (`height: 27px;`)
+const styles: StylesConfig = {
+  control: (styles) => ({
+    ...styles,
+    minWidth: 120,
+    color: 'var(--theia-foreground)',
+  }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    padding: 0,
+  }),
+  indicatorSeparator: () => ({
+    display: 'none',
+  }),
+  indicatorsContainer: () => ({
+    padding: '0px 5px',
+  }),
+  menu: (styles) => ({
+    ...styles,
+    marginTop: 0,
+  }),
+};
+const theme: ThemeConfig = (theme) => ({
+  ...theme,
+  borderRadius: 0,
+  spacing: {
+    controlHeight,
+    baseUnit: 2,
+    menuGutter: 4,
+  },
+  colors: {
+    ...theme.colors,
+    // `primary50`??? it's crazy but apparently, without this, we would get a light-blueish
+    // color when selecting an option in the select by clicking and then not releasing the button.
+    // https://react-select.com/styles#overriding-the-theme
+    primary50: 'var(--theia-list-activeSelectionBackground)',
+  },
+});
+const DropdownIndicator = () => <span className="fa fa-caret-down caret" />;
 
 export class ArduinoSelect<
-  T,
+  Option,
   IsMulti extends boolean = false,
-  Group extends GroupBase<T> = GroupBase<T>
-> extends Select<T, IsMulti, Group> {
-  constructor(props: Readonly<Props<T, IsMulti, Group>>) {
+  Group extends GroupBase<Option> = GroupBase<Option>
+> extends React.Component<StateManagerProps<Option, IsMulti, Group>> {
+  constructor(props: StateManagerProps<Option, IsMulti, Group>) {
     super(props);
   }
 
-  override render(): JSX.Element {
-    const controlHeight = 27; // from `monitor.css` -> `.serial-monitor-container .head` (`height: 27px;`)
-    const styles: StylesConfig<T, IsMulti, Group> = {
-      control: (styles) => ({
-        ...styles,
-        minWidth: 120,
-        color: 'var(--theia-foreground)',
-      }),
-      dropdownIndicator: (styles) => ({
-        ...styles,
-        padding: 0,
-      }),
-      indicatorSeparator: () => ({
-        display: 'none',
-      }),
-      indicatorsContainer: () => ({
-        padding: '0px 5px',
-      }),
-      menu: (styles) => ({
-        ...styles,
-        marginTop: 0,
-      }),
-    };
-    const theme: ThemeConfig = (theme) => ({
-      ...theme,
-      borderRadius: 0,
-      spacing: {
-        controlHeight,
-        baseUnit: 2,
-        menuGutter: 4,
-      },
-      colors: {
-        ...theme.colors,
-        // `primary50`??? it's crazy but apparently, without this, we would get a light-blueish
-        // color when selecting an option in the select by clicking and then not releasing the button.
-        // https://react-select.com/styles#overriding-the-theme
-        primary50: 'var(--theia-list-activeSelectionBackground)',
-      },
-    });
-    const DropdownIndicator = () => <span className="fa fa-caret-down caret" />;
+  override render(): React.ReactNode {
     return (
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
       <Select
         {...this.props}
         className="theia-select"
