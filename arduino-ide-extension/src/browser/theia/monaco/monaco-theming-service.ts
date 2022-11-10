01ee18b800aa27a2758896294cd7cea2efce20fc
Akos Kitta <a.kitta@arduino.cc>
2022-11-10 11:00:34 +0100

    reuse theme constants
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/browser/theia/monaco/monaco-theming-service.ts
+++ arduino-ide-extension/src/browser/theia/monaco/monaco-theming-service.ts
@@ -1,19 +1,21 @@
 import { injectable } from '@theia/core/shared/inversify';
 import { MonacoThemingService as TheiaMonacoThemingService } from '@theia/monaco/lib/browser/monaco-theming-service';
+import { ArduinoThemes } from '../core/theming';
 
 @injectable()
 export class MonacoThemingService extends TheiaMonacoThemingService {
   override initialize(): void {
     super.initialize();
+    const { Light, Dark } = ArduinoThemes;
     this.registerParsedTheme({
-      id: 'arduino-theme',
-      label: 'Light (Arduino)',
+      id: Light.id,
+      label: Light.label,
       uiTheme: 'vs',
       json: require('../../../../src/browser/data/default.color-theme.json'),
     });
     this.registerParsedTheme({
-      id: 'arduino-theme-dark',
-      label: 'Dark (Arduino)',
+      id: Dark.id,
+      label: Dark.label,
       uiTheme: 'vs-dark',
       json: require('../../../../src/browser/data/dark.color-theme.json'),
     });
