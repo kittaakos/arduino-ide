c42a34ea3d92f6190457662d05e1238c375a1a31
Akos Kitta <a.kitta@arduino.cc>
2023-04-27 17:41:42 +0200

    fix: docMain patching: dispose original
    Signed-off-by: Akos Kitta <a.kitta@arduino.cc>

--- arduino-ide-extension/src/browser/theia/plugin-ext/hosted-plugin.ts
+++ arduino-ide-extension/src/browser/theia/plugin-ext/hosted-plugin.ts
@@ -1,58 +1,22 @@
-import {
-  Emitter,
-  Event,
-  JsonRpcProxy,
-  UntitledResourceResolver,
-} from '@theia/core';
-import { ApplicationShell, OpenerService } from '@theia/core/lib/browser';
+import { Emitter, Event, JsonRpcProxy } from '@theia/core';
 import { injectable, interfaces } from '@theia/core/shared/inversify';
-import { EditorManager } from '@theia/editor/lib/browser';
 import * as monaco from '@theia/monaco-editor-core';
-import { MonacoBulkEditService } from '@theia/monaco/lib/browser/monaco-bulk-edit-service';
 import { MonacoEditorModel } from '@theia/monaco/lib/browser/monaco-editor-model';
-import { MonacoEditorService } from '@theia/monaco/lib/browser/monaco-editor-service';
-import { MonacoLanguages } from '@theia/monaco/lib/browser/monaco-languages';
 import { PLUGIN_RPC_CONTEXT } from '@theia/plugin-ext/lib/common/plugin-api-rpc';
 import { HostedPluginServer } from '@theia/plugin-ext/lib/common/plugin-protocol';
-import { RPCProtocol } from '@theia/plugin-ext/lib/common/rpc-protocol';
+import { RpcInvocationHandler } from '@theia/plugin-ext/lib/common/proxy-handler';
+import {
+  RPCProtocol,
+  RPCProtocolImpl,
+} from '@theia/plugin-ext/lib/common/rpc-protocol';
 import {
   HostedPluginSupport as TheiaHostedPluginSupport,
   PluginHost,
 } from '@theia/plugin-ext/lib/hosted/browser/hosted-plugin';
 import { PluginWorker } from '@theia/plugin-ext/lib/hosted/browser/plugin-worker';
 import { DocumentsMainImpl } from '@theia/plugin-ext/lib/main/browser/documents-main';
-import { EditorsAndDocumentsMain } from '@theia/plugin-ext/lib/main/browser/editors-and-documents-main';
 import { setUpPluginApi } from '@theia/plugin-ext/lib/main/browser/main-context';
-import { EditorModelService } from '@theia/plugin-ext/lib/main/browser/text-editor-model-service';
-import { TextEditorsMainImpl } from '@theia/plugin-ext/lib/main/browser/text-editors-main';
 import { MonitorUri } from '../../serial/monitor/monitor-uri';
-
-const originalOnModelAdded = DocumentsMainImpl.prototype['onModelAdded'];
-const originalOnModelRemoved = DocumentsMainImpl.prototype['onModelRemoved'];
-const originalOnModelChanged = DocumentsMainImpl.prototype['onModelChanged'];
-DocumentsMainImpl.prototype['onModelAdded'] = function (event: {
-  model: MonacoEditorModel;
-  oldModeId: string;
-}) {
-  if (event.model.uri.toString() === MonitorUri.toString()) {
-    return;
-  }
-  originalOnModelAdded.bind(this)(event);
-};
-DocumentsMainImpl.prototype['onModelRemoved'] = function (uri: monaco.Uri) {
-  if (uri.toString() === MonitorUri.toString()) {
-    return;
-  }
-  return originalOnModelRemoved.bind(this)(uri);
-};
-DocumentsMainImpl.prototype['onModelChanged'] = function (
-  model: MonacoEditorModel
-) {
-  if (model.uri.toString() === MonitorUri.toString()) {
-    return;
-  }
-  originalOnModelChanged.bind(this)(model);
-};
 
 @injectable()
 export class HostedPluginSupport extends TheiaHostedPluginSupport {
@@ -98,30 +62,36 @@ export class HostedPluginSupport extends TheiaHostedPluginSupport {
   }
 
   private patchDocumentsMain(rpc: RPCProtocol): void {
-    const editorsAndDocuments = new EditorsAndDocumentsMain(
-      rpc,
-      this.container
+    const handler: RpcInvocationHandler = (<RPCProtocolImpl>rpc)['locals'].get(
+      PLUGIN_RPC_CONTEXT.DOCUMENTS_MAIN.id
     );
+    const documentsMain: DocumentsMainImpl = handler.target;
 
-    const documentsMain = new DocumentsMainImpl(
-      editorsAndDocuments,
-      this.container.get<EditorModelService>(EditorModelService),
-      rpc,
-      this.container.get<EditorManager>(EditorManager),
-      this.container.get<OpenerService>(OpenerService),
-      this.container.get<ApplicationShell>(ApplicationShell),
-      this.container.get<UntitledResourceResolver>(UntitledResourceResolver),
-      this.container.get<MonacoLanguages>(MonacoLanguages)
-    );
-    rpc.set(PLUGIN_RPC_CONTEXT.DOCUMENTS_MAIN, documentsMain);
+    const originalOnModelAdded = documentsMain['onModelAdded'];
+    documentsMain['onModelAdded'] = function (event: {
+      model: MonacoEditorModel;
+      oldModeId: string;
+    }) {
+      if (event.model.uri.toString() === MonitorUri.toString()) {
+        return;
+      }
+      originalOnModelAdded.bind(this)(event);
+    };
 
-    const editorsMain = new TextEditorsMainImpl(
-      editorsAndDocuments,
-      documentsMain,
-      rpc,
-      this.container.get<MonacoBulkEditService>(MonacoBulkEditService),
-      this.container.get<MonacoEditorService>(MonacoEditorService)
-    );
-    rpc.set(PLUGIN_RPC_CONTEXT.TEXT_EDITORS_MAIN, editorsMain);
+    const originalOnModelChanged = documentsMain['onModelChanged'];
+    documentsMain['onModelChanged'] = function (model: MonacoEditorModel) {
+      if (model.uri.toString() === MonitorUri.toString()) {
+        return;
+      }
+      originalOnModelChanged.bind(this)(model);
+    };
+
+    const originalOnModelRemoved = documentsMain['onModelRemoved'];
+    documentsMain['onModelRemoved'] = function (uri: monaco.Uri) {
+      if (uri.toString() === MonitorUri.toString()) {
+        return;
+      }
+      return originalOnModelRemoved.bind(documentsMain)(uri);
+    };
   }
 }
